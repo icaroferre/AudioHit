fc91f1b222e5a4cce2765e2d0dae1ac8408bd700
Icaro Ferre <icaroferre+bitbucket@gmail.com>
2018-06-18 19:30:28 -0300

    Fixed folder detection and added preset functionality (still not 100% implemented)

--- code/src/main.rs
+++ code/src/main.rs
@@ -16,6 +16,13 @@ extern crate decibel;
 extern crate stopwatch;
 
 
+struct Preset {
+    fade_in: String,
+    fade_out: String,
+    thresh_db: String,
+}
+
+
 fn stof(s:&str) -> f32 {
 	let x = s.to_string().parse().unwrap();
 	x
@@ -25,15 +32,18 @@ fn main() {
 	//Version number
     let versionnumber = "0.1.0";
 
+    // Set default preset
+    let  default = getpreset(0);
+    
     // Start timer
     let sw = Stopwatch::start_new();
-
+    getpreset(1);
     // Initialize variables
 	let mut filename = "0".to_string();
     let mut folder_path = "0".to_string();
-	let mut fade_in_ms = "0.2".to_string();
-	let mut fade_out_ms = "2".to_string();
-	let mut thresh_db = "-36".to_string();
+	let mut fade_in_ms = default.0;
+	let mut fade_out_ms = default.1;
+	let mut thresh_db = default.2;
 
 	{  // Get variable values from arguments
         let mut ap = ArgumentParser::new();
@@ -75,15 +85,16 @@ fn main() {
     }
     else {
         // Folder batch processing
-        if folder_path != "0" {
-            let check_file: &Path = &filename.as_ref();
+        if &folder_path != "0" {
+            let check_file: &Path = &folder_path.as_ref();
 
             // Validate directory
             if check_file.is_dir() {
 
                 // Get list of files
-                let paths = fs::read_dir(folder_path).unwrap();
+                let paths = fs::read_dir(&folder_path).unwrap();
                 for path in paths {
+                    // Get file info (path, name, and extension)
                     let file_path = path.unwrap().path();
                     let file_name = &file_path.file_name();
                     let file_ext = match &file_path.extension(){
@@ -102,7 +113,7 @@ fn main() {
                 }
             }
             else {
-                println!("ERROR: Folder not found.");
+                println!("ERROR: Folder not found: {:?}", check_file);
             }
         }
     };
@@ -127,6 +138,7 @@ fn process(filename:&String, fade_in_ms:&String, fade_out_ms:&String, thresh_db:
     let file_chs = reader.spec().channels;
     let file_bits = reader.spec().bits_per_sample;
 
+    // Check if file is mono
     let stereo_file = match file_chs {
     	1 => false,
     	2 => true,
@@ -149,32 +161,41 @@ fn process(filename:&String, fade_in_ms:&String, fade_out_ms:&String, thresh_db:
     	_ => 0
     };
 
+    // Convert dB to linear amplitude
     let thresh_value: AmplitudeRatio<_> = DecibelRatio(stof(&thresh_db)).into();
     let thresh:f32 = max as f32 * thresh_value.amplitude_value() as f32;
-    // println!("Max amplitude in {} bits: {}", file_bits, max);
     println!("Threshold set to: {} ({})", thresh, thresh_value.amplitude_value());
+
 
     let mut start_point:u32 = 0;
     let mut end_point:u32 = 0;
     let samples: Vec<i16> = reader.samples().map(|s| s.unwrap()).collect();
 
     if stereo_file == false {
+        // Scan through audio file
     	for i in 0..file_dur as usize {
     		let samp_pos = i;
+
+            // Check if sample exists
     		let sample_exists = match samples.get(samp_pos + 1) {
     			Some(_) => true,
     			None => false
     		};
     		if sample_exists == true{
+
+                // Get absolute value of sample at index
     			let abs_val = match samples[samp_pos] {
     				_ if (samples[samp_pos] > 0) => samples[samp_pos],
     				_ if (samples[samp_pos] < 0) => samples[samp_pos] * -1,
     				_ => 0
     			};
-    			// println!("{} / {} / {}", samp_pos, abs_val, thresh);
+
+                // If start point has not been found and value is > thresh, set startpoint
     			if abs_val as f32 > thresh && start_point == 0 {
                 	start_point = samp_pos as u32;
             	}
+
+                // Keep updating the end point while sample value > thresh
             	if abs_val as f32 > thresh{
                 	end_point = samp_pos as u32;
             	}
@@ -185,30 +206,28 @@ fn process(filename:&String, fade_in_ms:&String, fade_out_ms:&String, thresh_db:
     } else {
     	println!("ERROR: The file is a stereo.");
     }
-    // println!(" ");
-    // println!("----------------------");
     println!(" ");
-
-
-	
-
     println!("Creating new file...");
     
 
  	// WRITER STAGE
 
+    // Set the specs of new audio file
  	let spec = hound::WavSpec {
         channels: 1,
         sample_rate: file_sr,
         bits_per_sample: file_bits,
         sample_format: hound::SampleFormat::Int,
     };
+
+    // Get parent folder of audio file
     let parentpath = &path.parent().unwrap().to_str().unwrap().to_string().to_owned();
     let mut new_filename  = parentpath.to_owned();
+
+    // Create output folder
     new_filename.push_str("/output/");
     new_filename = createdir(new_filename);
    	new_filename.push_str(&path.file_name().unwrap().to_str().unwrap());
-
     let new_file: &Path = new_filename.as_ref();
     println!("New file: {}", new_filename);
 
@@ -229,18 +248,25 @@ fn process(filename:&String, fade_in_ms:&String, fade_out_ms:&String, thresh_db:
 
     
 
-
+    // Start writing new file
     for i in 0..new_file_dur as usize  {
-    	let index = i + start_point as usize;
-    	let sample_exists = match samples.get(index) {
+
+        // Add start point to index
+    	let index = i + start_point as usize; 
+    	
+        // Check if sample is valid
+        let sample_exists = match samples.get(index) {
     			Some(_) => true,
     			None => false
     	};
     	if sample_exists == true{ 
 	        let amplitude = match i {
+                // Fade in
 	        	_ if (i < fade_in as usize) => (samples[index] as f32 * (i as f32 / fade_in as f32)) as i16,
-	        	_ if (i > (new_file_dur as i32 - fade_out) as usize) => (samples[index] as f32 * (1.0 - ((i as f32 - (new_file_dur as f32 - fade_out as f32) as f32) as f32 / fade_out as f32))) as i16,
-	        	_ => samples[index]
+	        	// Fade Out
+                _ if (i > (new_file_dur as i32 - fade_out) as usize) => (samples[index] as f32 * (1.0 - ((i as f32 - (new_file_dur as f32 - fade_out as f32) as f32) as f32 / fade_out as f32))) as i16,
+	        	// In between
+                _ => samples[index]
 	        };
 	        // println!("{} / {}: {}", i, new_file_dur, amplitude);
 	        writer.write_sample(amplitude as i16).unwrap();
@@ -259,5 +285,32 @@ fn createdir(dir:String) -> String {
     fs::create_dir(&dir);
     // Ok(()); 
     dir
+}
+
+fn getpreset(pnum:usize) -> (String, String, String) {
+    let drums = Preset{fade_in: "0.2".to_string(), fade_out: "2".to_string(), thresh_db: "-18".to_string()};
+    let bass = Preset{fade_in: "2".to_string(), fade_out: "2".to_string(), thresh_db: "-18".to_string()};
+
+    let mut presetvec = Vec::new();
+    presetvec.push(drums);
+    presetvec.push(bass);
+
+    // println!("{:?}", presetvec[pnum].fade_in);
+
+    let validpreset = match pnum {
+        _ if pnum < presetvec.len() => true,
+        _ if pnum  > presetvec.len() => false,
+        _ => false
+    };
+
+    assert_eq!(validpreset, true);
+
+    let fadein = &presetvec[pnum].fade_in;
+    let fadeout= &presetvec[pnum].fade_out;
+    let thresh = &presetvec[pnum].thresh_db;
+
+    (fadein.to_string(), fadeout.to_string(), thresh.to_string())
+
+    // println!("Bass preset: {}, {}, {}", presetvalues.0, presetvalues.1, presetvalues.2);
 
 }
